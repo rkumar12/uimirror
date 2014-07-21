3e2e71c2569b8527425f916a3085a11c1f72f6ce
Jayaram <jayaramimca@gmail.com>
2014-07-21 21:43:49 +0530

    Updated intialization

---
+++ uimirror/ws-security/src/main/java/com/uimirror/ws/api/security/conf/SecurityBeanIntitializer.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Uimirror.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Uimirror license
+ * which accompanies this distribution, and is available at
+ * http://www.uimirror.com/legal
+ *
+ * Contributors:
+ * Uimirror Team
+ *******************************************************************************/
+package com.uimirror.ws.api.security.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.uimirror.ws.api.security.service.ClientSecurityService;
+import com.uimirror.ws.api.security.service.UimClientSecurityServiceImpl;
+
+/**
+ * <p>Configures and register the beans 
+ * @author Jayaram
+ */
+@Configuration
+public class SecurityBeanIntitializer {
+	
+	@Bean
+	public ClientSecurityService clientSecurityService(){
+		return new UimClientSecurityServiceImpl();
+	}
+
+}
