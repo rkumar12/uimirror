8676f9c9c48abbf703381649e71b5c8794d3ff7b
Jay <jayaramimca@gmail.com>
2014-08-08 00:47:12 +0530

    accesstoken

--- uimirror/ws-resource-security/src/main/java/com/uimirror/ws/api/security/service/AccessTokenService.java
+++ uimirror/ws-resource-security/src/main/java/com/uimirror/ws/api/security/service/AccessTokenService.java
@@ -12,6 +12,7 @@ package com.uimirror.ws.api.security.service;
 
 import com.uimirror.ws.api.security.bean.base.AccessToken;
 import com.uimirror.ws.api.security.exception.AccessTokenException;
+import com.uimirror.ws.api.security.exception.ClientException;
 
 
 /**
@@ -30,7 +31,7 @@ public interface AccessTokenService {
 	 * @throws AccessTokenException if no token found, error code 404 for if no token exist else 500 for Database error
 	 * @throws IllegalArgumentException if input_parameter is not correct
 	 */
-	public AccessToken getAccessTokenDetailsByTokenId(final String token) throws AccessTokenException, IllegalArgumentException;
+	public AccessToken getAccessTokenDetailsByTokenId(final String token) throws AccessTokenException, IllegalArgumentException, ClientException;
 	
 	/**
 	 * <p>This is similar to the {@link #getAccessTokenDetailsByTokenId(String)}, where as this will return if token is valid</p>
@@ -38,8 +39,9 @@ public interface AccessTokenService {
 	 * @return {@link AccessToken} if a valid token found
 	 * @throws AccessTokenException if no valid token found, error code 404 for if no token exist, 419 if token is expired else 500 for Database error
 	 * @throws IllegalArgumentException if input_parameter is not correct
+	 * @throws ClientException while reteriving client info
 	 */
-	public AccessToken getActiveAccessTokenDetailsByTokenId(final String token) throws AccessTokenException, IllegalArgumentException;
+	public AccessToken getActiveAccessTokenDetailsByTokenId(final String token) throws AccessTokenException, IllegalArgumentException, ClientException;
 	
 	/**
 	 * <p>This will load the Access Key, client_id and user_id 
