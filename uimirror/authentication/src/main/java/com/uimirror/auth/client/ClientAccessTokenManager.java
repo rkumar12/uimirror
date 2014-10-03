e33a43620b742b38f348b6f27c63d68bf380e442
Jay <jayaramimca@gmail.com>
2014-10-04 04:16:12 +0530

    AccessToken Store

---
+++ uimirror/authentication/src/main/java/com/uimirror/auth/client/ClientAccessTokenManager.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Uimirror.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Uimirror license
+ * which accompanies this distribution, and is available at
+ * http://www.uimirror.com/legal
+ *
+ * Contributors:
+ * Uimirror Team
+ *******************************************************************************/
+package com.uimirror.auth.client;
+
+import com.uimirror.auth.manager.AbstractAccessTokenValidateManager;
+import com.uimirror.core.auth.bean.AccessToken;
+import com.uimirror.core.auth.bean.AuthenticatedDetails;
+import com.uimirror.core.auth.bean.Authentication;
+import com.uimirror.core.auth.dao.AccessTokenStore;
+
+/**
+ * AccessTokenManager that will be responsible to generate the {@linkplain AccessToken} 
+ * only for the user
+ * @author Jay
+ */
+public class ClientAccessTokenManager extends AbstractAccessTokenValidateManager{
+
+	/**
+	 * @param store
+	 */
+	public ClientAccessTokenManager(AccessTokenStore store) {
+		super(store);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see com.uimirror.core.auth.AccessTokenManager#generateToken(com.uimirror.core.auth.bean.Authentication, com.uimirror.core.auth.bean.AuthenticatedDetails)
+	 */
+	@Override
+	public AccessToken generateToken(Authentication auth,
+			AuthenticatedDetails authDetails) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
---
+++ uimirror/authentication/src/main/java/com/uimirror/auth/manager/AbstractAccessTokenExpireManager.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Uimirror.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Uimirror license
+ * which accompanies this distribution, and is available at
+ * http://www.uimirror.com/legal
+ *
+ * Contributors:
+ * Uimirror Team
+ *******************************************************************************/
+package com.uimirror.auth.manager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
+
+import com.uimirror.core.auth.AccessTokenManager;
+import com.uimirror.core.auth.bean.AccessToken;
+import com.uimirror.core.auth.bean.Authentication;
+import com.uimirror.core.auth.dao.AccessTokenStore;
+
+/**
+ * An implementation for the {@linkplain AccessToken} expiry handler
+ * @author Jay
+ */
+public abstract class AbstractAccessTokenExpireManager implements AccessTokenManager {
+
+	protected static final Logger LOG = LoggerFactory.getLogger(AccessTokenManager.class);
+	private AccessTokenStore store;
+
+	/**
+	 * 
+	 * @param store
+	 */
+	public AbstractAccessTokenExpireManager(AccessTokenStore store) {
+		Assert.notNull(store, "AccessTokenStore Can't be empty");
+		this.store = store;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.uimirror.core.auth.AccessTokenManager#markAsExpired(com.uimirror.core.auth.bean.Authentication, boolean)
+	 */
+	@Override
+	public void markAsExpired(Authentication auth, boolean async) {
+		validateParam(auth);
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see com.uimirror.core.auth.AccessTokenManager#markAsExpired(java.lang.String, boolean)
+	 */
+	@Override
+	public void markAsExpired(String token, boolean async) {
+		validateParam(token);
+	}
+	
+	private void validateParam(Object obj){
+		Assert.notNull(obj, "Authetication to expiry can't be empty");
+	}
+
+	public AccessTokenStore getStore() {
+		return store;
+	}
+	
+}
