9ffebc3db640de034d59e333560dfdb57ab00656
Jay <jayaramimca@gmail.com>
2014-10-20 23:15:01 +0530

    added test

--- uimirror/authentication/src/test/java/com/uimirror/account/auth/endpoint/ClientAuthenticationEndPointTest.java
+++ uimirror/authentication/src/test/java/com/uimirror/account/auth/endpoint/ClientAuthenticationEndPointTest.java
@@ -21,6 +21,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -33,8 +34,11 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import com.uimirror.account.StartApp;
+import com.uimirror.core.auth.AuthConstants;
 
 /**
  * Integration test case for the {@link ClientAuthenticationEndPoint}
@@ -70,6 +74,39 @@ public class ClientAuthenticationEndPointTest {
 		rst.setRequestFactory(s);
 		rst.setMessageConverters(messageConverters);
 		ResponseEntity<String> entity = rst.getForEntity("http://127.0.0.1:8080/uim/account/auth/oauth2/secret?cb=print&client_id=123&scope=r&redirect_uri=http://uimirror.com", String.class);
+		Assert.assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
+	
+	@Test
+	public void requestAccessTokenTest() {
+		MultiValueMap<String, String> param = new LinkedMultiValueMap<String, String>();
+		param.add(AuthConstants.CLIENT_SECRET_CODE, "1234");
+		param.add(AuthConstants.REDIRECT_URI, "http://uimirror.com");
+		param.add(AuthConstants.CLIENT_ID, "1234");
+		param.add(AuthConstants.CLIENT_SECRET, "1234as");
+		SimpleClientHttpRequestFactory s = new SimpleClientHttpRequestFactory() {
+			@Override
+			protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws IOException {
+				super.prepareConnection(connection, httpMethod);
+				connection.setConnectTimeout(4000);
+				connection.setReadTimeout(4000);
+				connection.setUseCaches(Boolean.TRUE);
+				connection.setRequestProperty("Accept-Encoding", "gzip,deflate");
+				//connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+			}
+		};
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers.set("Authorization", "Bearer 1234");
+		List<HttpMessageConverter<?> > messageConverters = new ArrayList< HttpMessageConverter<?> >(2);   
+		messageConverters.add( new StringHttpMessageConverter() );
+		messageConverters.add( new ByteArrayHttpMessageConverter() );
+		messageConverters.add(new FormHttpMessageConverter());
+		TestRestTemplate rst = new TestRestTemplate();
+		rst.setRequestFactory(s);
+		rst.setMessageConverters(messageConverters);
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(param, headers);
+		ResponseEntity<String> entity = rst.postForEntity("http://127.0.0.1:8080/uim/account/auth/oauth2/token", request, String.class);
 		Assert.assertEquals(HttpStatus.OK, entity.getStatusCode());
 	}
 
