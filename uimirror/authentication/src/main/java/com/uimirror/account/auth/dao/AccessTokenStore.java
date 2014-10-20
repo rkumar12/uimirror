d412161d90adeb479b2dc285f4f70c948603f8c5
Jay <jayaramimca@gmail.com>
2014-10-20 13:24:21 +0530

    Removed uncessary Dao

--- uimirror/authentication/src/main/java/com/uimirror/account/auth/dao/AccessTokenStore.java
+++ uimirror/authentication/src/main/java/com/uimirror/account/auth/dao/AccessTokenStore.java
@@ -55,85 +55,6 @@ public interface AccessTokenStore {
 	 * @throws DBException
 	 */
 	List<AccessToken> getByOwner(String ownerId) throws DBException;
-
-	/**
-	 * Retrieves the list of active {@linkplain AccessToken} issued to the owner
-	 * returns <code>null</code> if no record found 
-	 * @param ownerId
-	 * @return
-	 * @throws DBException
-	 */
-	List<AccessToken> getActivesByOwner(String ownerId) throws DBException;
-	
-	/**
-	 * Retrieves the list of {@linkplain AccessToken} issued to the client.
-	 * returns <code>null</code> if no record found
-	 * 
-	 * @param clientId
-	 * @return
-	 * @throws DBException
-	 */
-	List<AccessToken> getByClient(String clientId) throws DBException;
-	
-	/**
-	 * Retrieves the list of active {@linkplain AccessToken} issued to a client.
-	 * returns <code>null</code> if no record found
-	 * 
-	 * @param clientId
-	 * @return
-	 * @throws DBException
-	 */
-	List<AccessToken> getActivesByClient(String clientId) throws DBException;
-	
-	/**
-	 * Update the documents on the basics of the owner
-	 * @param ownerId
-	 * @param update
-	 * @return number of record got updated
-	 * @throws DBException
-	 */
-	int updateByOwner(String ownerId, Map<String, Object> update) throws DBException;
-	
-	/**
-	 * Update the documents on the basics of the client.
-	 * @param clientId
-	 * @param update
-	 * @return number of record got updated
-	 * @throws DBException
-	 */
-	int updateByClient(String clientId, Map<String, Object> update) throws DBException;
-	
-	/**
-	 * Delete the document based on the owner ID
-	 * @param ownerId
-	 * @return number of document got deleted
-	 * @throws DBException
-	 */
-	int deleteByOwner(String ownerId) throws DBException;
-	
-	/**
-	 * Delete all the expired token issued for that owner
-	 * @param ownerId
-	 * @return number of record got deleted
-	 * @throws DBException
-	 */
-	int deleteAllExpiredByOwner(String ownerId) throws DBException;
-	
-	/**
-	 * Delete all the token issued to the client specified.
-	 * @param clientId
-	 * @return
-	 * @throws DBException
-	 */
-	int deleteByClient(String clientId) throws DBException;
-	
-	/**
-	 * Delete all the expired token issued to the client specified.
-	 * @param clientId
-	 * @return
-	 * @throws DBException
-	 */
-	int deleteAllExpiredByClient(String clientId) throws DBException;
 	
 	/**
 	 * Delete documents based on the search criteria
@@ -142,20 +63,6 @@ public interface AccessTokenStore {
 	 * @throws DBException
 	 */
 	int deleteByQuery(Map<String, Object> query) throws DBException;
-	
-	/**
-	 * Mark all the token issued to the user as expired
-	 * @param ownerId
-	 * @throws DBException
-	 */
-	void markAllExpired(String ownerId) throws DBException;
-	
-	/**
-	 * Mark all the token issued for the client as expired
-	 * @param clientId
-	 * @throws DBException
-	 */
-	void markAllExpiredByClient(String clientId)throws DBException;
 	
 	/**
 	 * Mark the token issued for the client as expired
