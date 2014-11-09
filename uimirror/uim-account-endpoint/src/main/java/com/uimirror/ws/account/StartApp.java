3d0973e9fdb1a6150d46261460b37bc03513bdec
Jay <jayaramimca@gmail.com>
2014-11-09 21:10:40 +0530

    updated tomcat properties

--- uimirror/uim-account-endpoint/src/main/java/com/uimirror/ws/account/StartApp.java
+++ uimirror/uim-account-endpoint/src/main/java/com/uimirror/ws/account/StartApp.java
@@ -21,7 +21,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
@@ -51,27 +50,24 @@ public class StartApp {
 
 	private static Logger LOG = LoggerFactory.getLogger(StartApp.class);
 	
-	@Value("${tomcat.port:8080}")
+	@Value("${port:8080}")
 	private int tomcatPort;
+	
+	@Value("${nio.port:8443}")
+	private int tomcatNioPort;
 
 	@Value("${tomcat.sessionTimeout:30}")
 	private int tomcatSessionTimeout;
 
 	@Value("${tomcat.contextPath:/uim}/${application.id:account}")
 	private String contextPath;
-	
-	@Bean
-	public ServerProperties serverProperties() {
-		ServerProperties serverProperties = new ServerProperties();
-		serverProperties.setSessionTimeout(tomcatSessionTimeout);
-		serverProperties.setContextPath(contextPath);
-		serverProperties.setPort(tomcatPort);
-		return serverProperties;
-	}
 
 	@Bean
 	public EmbeddedServletContainerFactory servletContainer() {
 		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+		tomcat.setPort(tomcatPort);
+		tomcat.setSessionTimeout(tomcatSessionTimeout);
+		tomcat.setContextPath(contextPath);
 		tomcat.addAdditionalTomcatConnectors(createSslConnector());
 		return tomcat;
 	}
@@ -84,7 +80,7 @@ public class StartApp {
 			File truststore = keystore;
 			connector.setScheme("https");
 			connector.setSecure(true);
-			connector.setPort(8443);
+			connector.setPort(tomcatNioPort);
 			protocol.setSSLEnabled(true);
 			protocol.setKeystoreFile(keystore.getAbsolutePath());
 			protocol.setKeystorePass("changeit");
