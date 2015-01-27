473b2799ff334ed884ac36a5caf206c60efee786
Nishu <Nishu@192.168.2.6>
2015-01-27 22:08:13 +0530

    #18-BasicInfoTest

--- uimirror/uim-core-user/src/test/java/com/uimirror/user/BasicInfoTest.java
+++ uimirror/uim-core-user/src/test/java/com/uimirror/user/BasicInfoTest.java
@@ -11,12 +11,10 @@
 package com.uimirror.user;
 
 import static com.uimirror.core.mongo.feature.BasicDBFields.ID;
-import static com.uimirror.core.shop.UserDBFields.ACCOUNT_STATE;
-import static com.uimirror.core.shop.UserDBFields.ACCOUNT_STATUS;
-import static com.uimirror.core.shop.UserDBFields.EMAIL;
-import static com.uimirror.core.shop.UserDBFields.FIRST_NAME;
-import static com.uimirror.core.shop.UserDBFields.GENDER;
-import static com.uimirror.core.shop.UserDBFields.LAST_NAME;
+import static com.uimirror.core.user.DOBDBFields.DOB_FORMAT;
+import static com.uimirror.core.user.MetaInfoDBFields.LOCALE;
+import static com.uimirror.core.user.UserDBFields.DATE_OF_BIRTH;
+import static com.uimirror.core.user.UserDBFields.META_INFO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
@@ -30,6 +28,16 @@ import org.junit.Test;
 
 import com.uimirror.core.AccountState;
 import com.uimirror.core.AccountStatus;
+import com.uimirror.core.meta.BloodGroup;
+import com.uimirror.core.meta.Currencies;
+import com.uimirror.core.meta.Locales;
+import com.uimirror.core.meta.TimeZones;
+import com.uimirror.core.user.BasicInfo;
+import com.uimirror.core.user.DOB;
+import com.uimirror.core.user.Gender;
+import com.uimirror.core.user.RelationShipStatus;
+import com.uimirror.core.user.UserDBFields;
+import com.uimirror.core.user.DOB.DOBBuilder;
 
 /**
  * @author Jay
@@ -41,34 +49,38 @@ public class BasicInfoTest {
 	
 	private Map<String, Object> im;
 	private Map<String, Object> im1;
+	private Map<String, Object> meta;
 	
 
 	@Before
 	public void setUp(){
+		DOB dob=new DOB.DOBBuilder("1988-03-12").addFormat("MMM dd uuuu").addLocale("en_US").build();
+		meta = new WeakHashMap<String, Object>();
+		meta.put(LOCALE, "en_UK");
 		info = new BasicInfo.BasicInfoBuilder(null).
 				addEmail("jayaramimca@gmail.com").
 				addFirstName("Jayaram").addGender(Gender.MALE).
-				addLastName("Pradhan").addState(AccountState.NEW).addStatus(AccountStatus.ACTIVE).build();
-		info1 = new BasicInfo.BasicInfoBuilder(null).
-				addEmail("jayaramimca@gmail.com").
-				addFirstName("Jayaram").addGender(Gender.MALE).
-				addLastName("Pradhan").addState(AccountState.NEW).addStatus(AccountStatus.ACTIVE).build();
+				addLastName("Pradhan").addState(AccountState.NEW).addBloodGroup(BloodGroup.APOSITIVE)
+				.addSnapLoc("test.jpg").addStatus(AccountStatus.ACTIVE).addTimeZone(TimeZones.DEFAULT).addLocale(Locales.ENGLISH).addCurrency(Currencies.RUPEE)
+				.addCoverSnapLoc("text.jpg").addRelationShipStatus(RelationShipStatus.NONE)
+				.addDOB(dob)
+				.build();
 		im = new WeakHashMap<String, Object>();
 //		im.put(ID, getId());
-		im.put(FIRST_NAME, "Jayaram");
-		im.put(LAST_NAME, "Pradhan");
-		im.put(EMAIL, "jayaramimca@gmail.com");
-		im.put(GENDER, Gender.MALE.getGender());
-		im.put(ACCOUNT_STATUS, AccountStatus.ACTIVE.getStatus());
-		im.put(ACCOUNT_STATE, AccountState.NEW.getState());
-		im1 = new WeakHashMap<String, Object>();
-//		im.put(ID, getId());
-		im1.put(FIRST_NAME, "Jayaram");
-		im1.put(LAST_NAME, "Pradhan");
-		im1.put(EMAIL, "jayaramimca@gmail.com");
-		im1.put(GENDER, Gender.MALE.getGender());
-		im1.put(ACCOUNT_STATUS, AccountStatus.ACTIVE.getStatus());
-		im1.put(ACCOUNT_STATE, AccountState.NEW.getState());
+		im.put(UserDBFields.FIRST_NAME, "Jayaram");
+		im.put(UserDBFields.LAST_NAME, "Pradhan");
+		im.put(UserDBFields.EMAIL, "jayaramimca@gmail.com");
+		im.put(UserDBFields.GENDER, Gender.MALE.getGender());
+		im.put(UserDBFields.TIME_ZONE, TimeZones.DEFAULT.getTimeZone().getID());
+		im.put(UserDBFields.ACCOUNT_STATUS, AccountStatus.ACTIVE.getStatus());
+		im.put(UserDBFields.ACCOUNT_STATE, AccountState.NEW.getState());
+		im.put(UserDBFields.BLOOD_GROUP, BloodGroup.APOSITIVE.getGroup());
+		im.put(UserDBFields.RELATION_SHIP_STATUS, RelationShipStatus.NONE.getStatus());
+		im.put(UserDBFields.SNAP_LOC, "test.jpg");
+		im.put(UserDBFields.COVER_SNAP_LOC, "text.jpg");
+		im.put(UserDBFields.LOCALE, Locales.ENGLISH.getLocale());
+		im.put(UserDBFields.CURRENCY, Currencies.RUPEE.getCurrency());
+		im.put(UserDBFields.DATE_OF_BIRTH, dob.toMap());
 	}
 	
 	@Test
@@ -79,18 +91,17 @@ public class BasicInfoTest {
 	@Test
 	public void serializeTest(){
 		assertThat(info.writeToMap()).isEqualTo(im);
-		assertThat(info1.writeToMap()).isEqualTo(im1);
 		im.put(ID, "123");
-		assertThat(info1.updateId("123").writeToMap()).isEqualTo(im);
+		assertThat(info.updateId("123").writeToMap()).isEqualTo(im);
 		im.remove(ID);
 	}
 	
 	@Test
 	public void deSerializeTest(){
+		im.put(META_INFO, meta);
 		assertThat(info.readFromMap(im)).isEqualTo(info);
-		assertThat(info.readFromMap(im1)).isEqualTo(info1);
 		im.put(ID, "123");
-		assertThat(info1.readFromMap(im)).isEqualTo(info.updateId("123"));
+		assertThat(info.readFromMap(im)).isEqualTo(info.updateId("123"));
 		im.remove(ID);
 	}
 }
